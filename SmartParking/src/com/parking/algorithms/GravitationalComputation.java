2bb0b8103276d1c5bf9b0e1bbac850da14a03f1e
Madan Gopal Venkatesan <madan1988@gmail.com>
2015-02-17 23:48:53 -0600

    Maze Solver Checkins

--- SmartParking/src/com/parking/algorithms/GravitationalComputation.java
+++
@@ -1,9 +0,0 @@
-package com.parking.algorithms;
-
-public class GravitationalComputation {
-
-	public double getGForce(final int numBlocks, final double distance) {
-		return numBlocks / (Math.pow(distance, 2));
-	}
-
-}
