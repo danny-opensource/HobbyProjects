9fd1491ab7e771deec12baecdf7cbfa92c6f7eb3
madan1988 <madan1988@gmail.com>
2015-04-04 20:30:46 -0500

    Distance Changes

--- ParkingWeb/src/com/parking/maps/TestBed.java
+++ ParkingWeb/src/com/parking/maps/TestBed.java
@@ -1,13 +1,24 @@
 package com.parking.maps;
 
+import java.math.BigDecimal;
+
 import com.parking.utils.DistanceUtils;
 
 public class TestBed {
 	public static void main(String[] args) {
 		double anotherDistVal = DistanceUtils.anotherDistance(37.806205, -122.424262,37.803293, -122.417202, 'M');
-		double googleDistance = DistanceUtils.distance(37.806205, -122.424262, 37.803293, -122.417202, 'M');
+	/*	double googleDistance = DistanceUtils.distance(37.806205, -122.424262, 37.803293, -122.417202, 'M');
 
-		System.out.println("Another Distance: " + anotherDistVal);
-		System.out.println("Google Distance: " + googleDistance);
+		//System.out.println("Another Distance: " + anotherDistVal);
+		//System.out.println("Google Distance: " + googleDistance);
+		
+		double totalDistance = DistanceUtils.distance(37.806205, -122.424262, 37.803293, -122.417202, 'M');
+		double totalTime = DistanceUtils.totalTime(37.806205, -122.424262, 37.803293, -122.417202, 'M');
+		System.out.println("Total Distance: " + totalDistance);
+		System.out.println("Total Time: " + totalTime);*/
+		
+		int x = 10;
+		double y = x/(double)100;
+		System.out.println(y);
 	}
 }
