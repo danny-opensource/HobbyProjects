2bcfdea26df490d59f11c7e6aeb9a6a0d463ad36
madan1988 <madan1988@gmail.com>
2015-04-05 22:10:54 -0500

    More utilities added

--- ParkingWeb/src/com/parking/utils/GeneralUtils.java
+++ ParkingWeb/src/com/parking/utils/GeneralUtils.java
@@ -3,12 +3,13 @@ package com.parking.utils;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.sql.Statement;
 import java.sql.Timestamp;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.TreeMap;
 
 import com.parking.constants.AppConstants;
 import com.parking.constants.AppConstants.ALGORITHM_TYPE;
@@ -16,7 +17,35 @@ import com.parking.model.Location;
 import com.parking.model.RoadNetworkEdge;
 import com.parking.model.RoadNetworkNode;
 
+/**
+ * All other utility functions that cannot be categorized under Database and
+ * Distance Utils are here.
+ * 
+ * @author Madan Gopal (Git: madan1988)
+ *
+ */
 public class GeneralUtils {
+
+	public static <K, V extends Comparable<V>> Map<K, V> sortByValues(final Map<K, V> map) {
+		Comparator<K> valueComparator = new Comparator<K>() {
+
+			@Override
+			public int compare(K k1, K k2) {
+				int compare = map.get(k1).compareTo(map.get(k2));
+				if (compare == 0)
+					return 1;
+				return compare;
+			}
+		};
+		Map<K, V> sortedByValues = new TreeMap<K, V>(valueComparator);
+		sortedByValues.putAll(map);
+		return sortedByValues;
+	}
+
+	public static int getNearestParkingBlock(Map<Integer, Double> minMap) {
+		Iterator<Integer> i = minMap.keySet().iterator();
+		return i.hasNext() ? i.next() : null;
+	}
 
 	public static int getBlockWithMaxForce(Map<Integer, Double> minMap) {
 
@@ -80,40 +109,64 @@ public class GeneralUtils {
 		try {
 			conn = DriverManager.getConnection("jdbc:postgresql://127.0.0.1:5432/postgres", "postgres", "cs440");
 			String query = "";
+			String secondQuery = "";
+			String timeStamp = "";
+			query = "select max(p.timestamp) from parking.\"projection\" p where p.block_id=" + block + "and p.timestamp <'" + driverTimeStamp
+					+ "' group by p.available";
+
+			stmt = conn.createStatement();
+			rs = stmt.executeQuery(query);
+			if (rs.next()) {
+				timeStamp = rs.getString(1);
+			}
+			rs.close();
+			stmt.close();
 			switch (congestionLevel) {
 			case 0:
-				query = "select p.available, max(p.timestamp) from parking.\"projection\" p where p.block_id=" + block + "and p.timestamp <'"
-						+ driverTimeStamp + "' group by p.available";
+
+				/*
+				 * query =
+				 * "select p.available, max(p.timestamp) from parking.\"projection\" p where p.block_id="
+				 * + block + "and p.timestamp <'" + driverTimeStamp +
+				 * "' group by p.available";
+				 */
+				secondQuery = "select p.available from parking.\"projection\" p where p.timestamp='" + timeStamp + "' and block_id=" + block;
+				// System.out.println("Getting Data for Congestion Level 0%: " +
+				// query);
 				break;
 			case 30:
-				query = "select p.available, max(p.timestamp) from parking.\"projection_thirty\" p where p.block_id=" + block + "and p.timestamp <'"
-						+ driverTimeStamp + "' group by p.available";
+
+				secondQuery = "select p.available from parking.\"projection_thirty\" p where p.timestamp='" + timeStamp + "' and block_id=" + block;
+				// System.out.println("Getting Data for Congestion Level 30%: "
+				// + query);
 				break;
 			case 50:
-				query = "select p.available, max(p.timestamp) from parking.\"projection_fifty\" p where p.block_id=" + block + "and p.timestamp <'"
-						+ driverTimeStamp + "' group by p.available";
+				// System.out.println("Getting Data for Congestion Level 50%");
+				secondQuery = "select p.available from parking.\"projection_fifty\" p where p.timestamp='" + timeStamp + "' and block_id=" + block;
 				break;
 			case 70:
-				query = "select p.available, max(p.timestamp) from parking.\"projection_seventy\" p where p.block_id=" + block + "and p.timestamp <'"
-						+ driverTimeStamp + "' group by p.available";
+				// System.out.println("Getting Data for Congestion Level 70%");
+				secondQuery = "select p.available from parking.\"projection_seventy\" p where p.timestamp='" + timeStamp + "' and block_id=" + block;
 				break;
 			case 90:
-				query = "select p.available, max(p.timestamp) from parking.\"projection_ninety\" p where p.block_id=" + block + "and p.timestamp <'"
-						+ driverTimeStamp + "' group by p.available";
+				// System.out.println("Getting Data for Congestion Level 90%");
+				secondQuery = "select p.available from parking.\"projection_ninety\" p where p.timestamp='" + timeStamp + "' and block_id=" + block;
 				break;
 			default:
-				query = "select p.available, max(p.timestamp) from parking.\"projection\" p where p.block_id=" + block + "and p.timestamp <'"
-						+ driverTimeStamp + "' group by p.available";
+				secondQuery = "select p.available from parking.\"projection\" p where p.timestamp='" + timeStamp + "' and block_id=" + block;
 				break;
 			}
 
 			stmt = conn.createStatement();
-			rs = stmt.executeQuery(query);
+			rs = stmt.executeQuery(secondQuery);
+
 			int parkingLotCount = 0;
 			if (rs.next()) {
-			//	System.out.println("*** Chosen timestamp: " + rs.getString(2));
 				parkingLotCount = Integer.parseInt(rs.getString(1));
 			}
+			/*if (block == 612311) {
+				System.out.println("612311 parking avail: " + parkingLotCount + " for congession: " + congestionLevel);
+			}*/
 			return parkingLotCount;
 		} catch (Exception ex) {
 			ex.printStackTrace();
