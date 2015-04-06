db8c4752c7c3aace623e450bbf93c0c7f5b52f75
madan1988 <madan1988@gmail.com>
2015-04-05 22:00:12 -0500

    Support for Deterministic Graph

--- ParkingWeb/src/com/parking/algorithms/GravitationalComputation.java
+++ ParkingWeb/src/com/parking/algorithms/GravitationalComputation.java
@@ -1,5 +1,11 @@
 package com.parking.algorithms;
 
+/**
+ * Gravitation Algorithm Implementer
+ * 
+ * @author Madan Gopal (Git: madan1988)
+ *
+ */
 public class GravitationalComputation {
 
 	public double getGForce(final int numBlocks, final double distance) {
