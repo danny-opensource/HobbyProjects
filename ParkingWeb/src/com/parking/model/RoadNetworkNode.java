e57368e10dd688a6e8495eb937dd45f942f13927
madan1988 <madan1988@gmail.com>
2015-04-05 22:09:47 -0500

    clean up

--- ParkingWeb/src/com/parking/model/RoadNetworkNode.java
+++ ParkingWeb/src/com/parking/model/RoadNetworkNode.java
@@ -1,5 +1,11 @@
 package com.parking.model;
 
+/**
+ * Road Network Node Data Structure
+ * 
+ * @author Madan Gopal (Git: madan1988)
+ *
+ */
 public class RoadNetworkNode {
 	public int nodeId;
 	public double latitude;
