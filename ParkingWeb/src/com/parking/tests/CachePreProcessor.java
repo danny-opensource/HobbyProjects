97b7e12df16b1116632f7d433c6ca4899e4d3fda
madan1988 <madan1988@gmail.com>
2015-05-02 10:32:43 -0500

    Test Suite

--- ParkingWeb/src/com/parking/tests/CachePreProcessor.java
+++ ParkingWeb/src/com/parking/tests/CachePreProcessor.java
@@ -6,14 +6,19 @@ import java.io.IOException;
 import java.util.Iterator;
 
 import com.parking.constants.AppConstants;
+import com.parking.utils.DistanceUtils;
 
 public class CachePreProcessor {
 
 	
+	
 
 	public static void main(String[] args) {
-		CachePreProcessor cachePreProcessor = new CachePreProcessor();
-	
+		double totalDistance = DistanceUtils.distance(37.795752,-122.39434, 37.8066485617,-122.4124854154, 'M');
+		double totalTime = DistanceUtils.totalTime(37.8077921947,-122.4194870247, 37.800136,-122.398633, 'M');
+		
+		System.out.println("Total Distance: " + totalDistance);
+		System.out.println("Total Time: " + totalTime);
 
 	}
 }
